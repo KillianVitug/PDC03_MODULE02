d6d4e623bd03c129c49d95d58294750da944d982
Vitug Killian <killianjarelvitug@yahoo.com>
2023-02-10 13:10:04 +0800

    First Commit

---
+++ PDC03_MODULE02/PDC03_MODULE02/Activity02.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03_MODULE02
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Activity02 : ContentPage
+    {
+        public Activity02()
+        {
+            InitializeComponent();
+        }
+
+        void onSlideValueChanged(object sender,ValueChangedEventArgs args)
+        {
+            lblValue.Text=args.NewValue.ToString("F3");
+        }
+    }
+}
